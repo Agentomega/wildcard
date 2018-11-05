e18108eb09a6a9fe10c262f76e405dc529fb96b2
Agentomega <wedgeantillies61@sbcglobal.net>
2018-11-04 23:52:44 -0500

    feature/vanilla: Add hopper subtypes.

---
+++ src/main/java/omega/wildcard/vanilla/hoppers/TileEntityEquivalenceHopper.java
@@ -0,0 +1,13 @@
+package omega.wildcard.vanilla.hoppers;
+
+import net.minecraft.item.ItemStack;
+
+public class TileEntityEquivalenceHopper extends TileEntityFilterHopper {
+
+	@Override
+	boolean matchesFilter(ItemStack stack) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/main/java/omega/wildcard/vanilla/hoppers/TileEntityRegexHopper.java
@@ -0,0 +1,13 @@
+package omega.wildcard.vanilla.hoppers;
+
+import net.minecraft.item.ItemStack;
+
+public class TileEntityRegexHopper extends TileEntityFilterHopper {
+
+	@Override
+	boolean matchesFilter(ItemStack stack) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
